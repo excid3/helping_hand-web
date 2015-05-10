bfab1b8a76fb999c8b418771a23a26b9a5ab7f64
Chris Oliver <excid3@gmail.com>
2015-05-09 22:35:53 -0400

    Update UI

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_x_frame_options, only: [:record]
+  layout "minimal", only: [:record]
 
   def show
     @exception = HelpingHand::Exception.find_by(class_name: params[:exception_id], message: params[:id])
@@ -8,6 +9,7 @@ class MessagesController < ApplicationController
   def record
     @exception = HelpingHand::Exception.where(class_name: params[:exception_id], message: params[:message_id]).first_or_create
     @exception.increment! :throws
+
     render action: :show
   end
 
